9d3f84412822dd558a62b8d864a2b6f11ed774f1
AndreeaMatei2811 <alm.matei@gmail.com>
2020-11-25 12:23:07 +0100

    add buttons with link in home page after user login

--- src/pages/homePage/HomePage.js
+++ src/pages/homePage/HomePage.js
@@ -1,5 +1,39 @@
 import React from "react";
+import Button from "react-bootstrap/Button";
+import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { selectToken } from "../../store/user/selectors";
 
 export default function HomePage() {
-  return <div>This is the home page</div>;
+  const token = useSelector(selectToken);
+  return (
+    <div>
+      <h3>This is the home page</h3>
+
+      {token ? (
+        <Link to={`/shopping-lists`}>
+          <Button>Shopping Lists</Button>
+        </Link>
+      ) : (
+        <div></div>
+        // <Link to={`/login`}>
+        //   <Button>Please log in</Button>
+        // </Link>
+      )}
+      {token ? (
+        <Link to={`/inventory`}>
+          <Button>Inventory</Button>
+        </Link>
+      ) : (
+        <div></div>
+      )}
+      {/* {token ? (
+        <Link to={`/budget`}>
+          <Button>Budget</Button>
+        </Link>
+      ) : (
+        <div></div>
+      )} */}
+    </div>
+  );
 }
